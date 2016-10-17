33fdec14143283a589e0af11679331988b5af909
svlassiev <svlassiev@gmail.com>
2016-10-17 17:16:04 +0300

    main.java package prefix o_O

--- src/main/java/com/mkyong/rest/JSONService.java
+++ src/main/java/com/mkyong/rest/JSONService.java
@@ -1,14 +1,10 @@
 package main.java.com.mkyong.rest;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import main.java.com.mkyong.Track;
+
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import com.mkyong.Track;
 
 @Path("/json/metallica")
 public class JSONService {
